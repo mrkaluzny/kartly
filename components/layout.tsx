60566a95f2fe4a89b6269c9fb8caecc9a78c630f
Hamed <hamedsedighi@gmail.com>
2023-04-09 13:21:17 +0200

    add footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,8 +1,10 @@
 import { PropsWithChildren } from "react";
+import Footer from "@/components/footer";
 
 const Layout = ({ children }: PropsWithChildren): JSX.Element => (
   <div className="container mx-auto px-5">
     <main>{children}</main>
+    <Footer />
   </div>
 );
 
